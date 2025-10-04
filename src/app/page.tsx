cc55a2e7b07d4db9356b6b34d5f7e34c25d3b358
iamvevaar <gautam15suthar@gmail.com>
2025-10-04 15:40:46 +0530

    Timeline + Contact us section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 "use client";
-import { HeroSection } from "@/components/hero-section";
+import { HeroSection } from "@/components/sections/hero-section";
+import JoinList from "@/components/sections/JoinList";
+import { TimelineSection } from "@/components/sections/timeline-section";
 
 export default function Home() {
+
   return (
     <main>
       <HeroSection />
+      <TimelineSection/>
+      <JoinList />      
     </main>
   );
 }
