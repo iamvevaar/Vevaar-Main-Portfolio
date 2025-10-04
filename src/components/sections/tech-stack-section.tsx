1c8549a948835e5ab4f34d7bbfb93133bfd0803a
iamvevaar <gautam15suthar@gmail.com>
2025-10-05 00:26:43 +0530

    Two Rows Added

--- src/components/sections/tech-stack-section.tsx
+++ src/components/sections/tech-stack-section.tsx
@@ -1,11 +1,20 @@
 import { InfiniteMovingCards } from "../ui/infinite-moving-cards";
 
 export function TechStackSection() {
+  const middleIndex = Math.ceil(icons.length / 2);
+  const firstRowIcons = icons.slice(0, middleIndex);
+  const secondRowIcons = icons.slice(middleIndex);
+
   return (
     <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
       <InfiniteMovingCards
-        items={icons}
+        items={firstRowIcons}
         direction="right"
+        speed="slow"
+      />
+      <InfiniteMovingCards
+        items={secondRowIcons}
+        direction="left"
         speed="slow"
       />
     </div>
