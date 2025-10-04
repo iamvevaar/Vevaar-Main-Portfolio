d69b8c44feae16c0dc271eba7210e65aad24707d
iamvevaar <gautam15suthar@gmail.com>
2025-10-04 14:14:53 +0530

    Audio + Assets Added

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,4 +1,20 @@
+import { useRef } from "react";
+
 export function HeroSection() {
+    const audioRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0; // reset to start
+    }
+  };
   return (
     <div className="h-screen bg-black w-full flex justify-center overflow-hidden">
       <video
@@ -7,7 +23,16 @@ export function HeroSection() {
         muted
         loop
       />
-     
+      <div
+        className="absolute bottom-48 left-1/2 transform -translate-x-1/2 flex flex-col justify-center items-center text-center h-44 w-44 rounded-full cursor-pointer"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+      </div>
+
+      {/* Hidden audio element */}
+      <audio ref={audioRef} src="/a.mp3" preload="auto" />
     </div>
+    
   );
 }
