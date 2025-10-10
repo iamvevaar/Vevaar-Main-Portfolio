c1929febd531c1716fcbdcad461e686ff018345c
iamvevaar <gautam15suthar@gmail.com>
2025-10-10 17:39:26 +0530

    Embeddd script handled for portfolio content

--- src/components/ui/portfolio-card.tsx
+++ src/components/ui/portfolio-card.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import { IconBrandYoutubeFilled } from "@tabler/icons-react";
 
@@ -23,6 +24,24 @@ export const PortfolioCard: React.FC<PortfolioCardProps> = ({
   youtubeLink,
   embeddedVideoUrl,
 }) => {
+  useEffect(() => {
+    if (embeddedVideoUrl) {
+      const scriptSrcMatch = embeddedVideoUrl.match(/src="([^"]+)"/);
+      if (scriptSrcMatch && scriptSrcMatch[1]) {
+        const scriptSrc = scriptSrcMatch[1];
+        const script = document.createElement("script");
+        script.src = scriptSrc;
+        script.async = true;
+        document.body.appendChild(script);
+
+        // Optional: Clean up the script when the component unmounts
+        return () => {
+          document.body.removeChild(script);
+        };
+      }
+    }
+  }, [embeddedVideoUrl]);
+
   return (
     <CardContainer className="inter-var">
       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
